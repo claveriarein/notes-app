cb6ed6cb81175ce38b56b5e083dcfae35ea0dff0
claveriarein <claveriarein039@gmail.com>
2026-04-03 14:24:51 +0800

    trust railway proxies to fix livewire assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Notes App' }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    @livewireStyles
 </head>
 <body class="antialiased bg-gray-50">
 
     {{ $slot }}
 
-    @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/livewire@3.x.x/dist/livewire.min.js" defer></script>
 </body>
 </html>
